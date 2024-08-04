bad396333427c9076715c186c71612d04e033b4d
srushti-pmr <srushtiparmar2210@gmail.com>
2024-08-04 00:00:35 -0400

    added dynamic user transactions data

--- finertia/views.py
+++ finertia/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout as auth_logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+from django.contrib.auth import  authenticate, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+
+
+# class SignupView(View):
+#     def get(self, request):
+#         form = UserCreationForm()
+#         return render(request, 'registration/signup.html', {'form': form})
+#
+#     def post(self, request):
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('finertia:dashboard')
+#         return render(request, 'registration/signup.html', {'form': form})
+
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.views import View
+from .models import UserData, AllTransactions
+import random
+from django.db.models import Sum, DecimalField
+from django.db.models.functions import Coalesce
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 class SignupView(View):
     def get(self, request):
@@ -14,8 +39,21 @@ class SignupView(View):
         if form.is_valid():
             user = form.save()
             login(request, user)
+
+            # Create UserData instance
+            user_data = UserData.objects.create(user=user)
+
+            # Get 15 random transactions
+            all_transactions = list(AllTransactions.objects.all())
+            random_transactions = random.sample(all_transactions, 15)
+
+            # Add transactions to user_data
+            user_data.transactions.add(*random_transactions)
+            print('here')
             return redirect('finertia:dashboard')
+
         return render(request, 'registration/signup.html', {'form': form})
+
 
 class LoginView(View):
     def get(self, request):
@@ -39,8 +77,20 @@ class LoginView(View):
 def home(request):
     return render(request, 'home.html')
 
+
+@login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    user_data = UserData.objects.get(user=request.user)
+    user_transactions = user_data.transactions.all()
+    context = {
+        'transactions': user_transactions,
+        'username': request.user.username  # Add this line
+    }
+    return render(request, 'dashboard.html', context)
+
+
+
+
 
 def analytics(request):
     return render(request, 'analytics.html')
@@ -49,11 +99,12 @@ def analytics(request):
 def insights(request):
     return render(request, 'ini-test.html')
 
+
 def payments(request):
     return render(request, 'payments.html')
 
-def logout(request):
 
+def logout(request):
     if request.method == 'GET':
         auth_logout(request)
         return render(request, 'registration/login.html')
