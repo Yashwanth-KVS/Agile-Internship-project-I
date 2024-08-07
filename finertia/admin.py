085de39671d7b80beac0c8bf524b000efb8205ee
anjanikumarkandula <anjanikumarkandula@gmail.com>
2024-08-07 17:26:10 -0400

    commit

--- finertia/admin.py
+++ finertia/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import AllTransactions, UserData
+from .models import AllTransactions, UserData,Card
 
 # Register your models here.
 
 admin.site.register(AllTransactions)
 admin.site.register(UserData)
+admin.site.register(Card)
